81aa694e6a257dcc311d77f3417f026bfe86fff6
DDGaonkar <gaonkardeepanshu@gmail.com>
2022-03-22 23:42:28 +0530

    Quicksort added

--- sorting/Python/QuickSort.py
+++ sorting/Python/QuickSort.py
@@ -4,7 +4,7 @@ Spyder Editor
 
 This is a temporary script file.
 """
-# Hoare partition scheme
+
 
 def swap(a, b, arr):
     if a!=b:
@@ -12,7 +12,8 @@ def swap(a, b, arr):
         arr[a] = arr[b]
         arr[b] = temp
         
-def partition(elements, start, end):
+# Hoare partition scheme        
+def hoare_partition(elements, start, end):
     pidx = start
     pivot = elements[pidx]
     
@@ -28,16 +29,47 @@ def partition(elements, start, end):
             swap(start, end, elements)
             
     swap(pidx, end, elements)
-    
+    2
     return end
+
+# Lomuto partition scheme
+def lomuto_partition(elements, low, high):
+    pivot = elements[high]
+    i = low - 1
+    
+    while low < high:
+        if elements[low] <= pivot:
+            i += 1
+            swap(low, i, elements)
+            low += 1
+        else:
+            low += 1
+    
+    i += 1
+    swap(low, i, elements)
+    return i
+            
     
 def quicksort(elements, start, end):
     if start < end:
-        pi = partition(elements, start, end)
+        if scheme == 1:
+            pi = hoare_partition(elements, start, end)
+        else:
+            pi = lomuto_partition(elements, start, end)
         quicksort(elements, start, pi-1)    # left partition
         quicksort(elements, pi+1, end)  # right partition
 
 if __name__ == '__main__':
-    elements = [11, 9, 29, 7, 2, 15, 28]
+    # elements = [11, 9, 29, 7, 2, 15, 28]
+    print("Enter list of elements in unsorted manner seperated by space")
+    
+    # 2 ways to take input from user
+    # way 1
+    # elements = [int(x) for x in input().split()]
+    # way 2
+    x = input()
+    elements = list(map(int, x.split()))
+    print("Enter 1 for Hoare partition scheme or 2 for Lomuto partition scheme")
+    scheme = int(input())
     quicksort(elements, 0, len(elements)-1)
     print(elements)
